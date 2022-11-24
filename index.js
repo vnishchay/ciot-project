81504507d55d8916b5d1ca0af95b54a2b3833afd
codernishchay <nishchay.vns2000@gmail.com>
2022-11-24 09:13:22 +0530

    make it better

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ import User from './model.js'
 import errorHandler from './errorMiddleWare.js'
 import userRouter from './routes.js'
 
+import cors from 'cors'
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+
+
 const PORT = process.env.PORT || 5000
 const app = express()
  
@@ -19,6 +29,7 @@ app.use('/api', userRouter)
 
 app.use(errorHandler)
 
+app.use(cors(corsOptions)) 
 app.listen(PORT, () => {
   console.log(`server running on  :  ${PORT}`)
 })
